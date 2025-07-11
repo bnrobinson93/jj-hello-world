1c07362aaaf0be0e93a7d405a903482c6415e064
Brad Robinson <deluxebrokepets@duck.com>
2025-07-10 23:19:54 -0500

    add a comment to main

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// A "Hello, world!" program written in Go
 package main
 
 import (
@@ -7,6 +8,7 @@ import (
 // This is the best way to print things out
 func main() {
 	print("Hello, world!")
+	print("Goodbye, world!")
 }
 
 // A function to print out a string
